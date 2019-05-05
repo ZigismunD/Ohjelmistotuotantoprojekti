9169bb99d1c4fcc658b1069b62d5edee263cac22
Sami Sikkilä <sami.sikkila@metropolia.fi>
2019-05-05 17:41:42 +0300

    InputValidator sekä tarkistukset myyntisivun asiakastietoihin

---
+++ src/main/java/view/InputValidator.java
@@ -0,0 +1,26 @@
+package view;
+
+public class InputValidator {
+    private static InputValidator INSTANCE = null;
+
+    private InputValidator() {
+
+    }
+
+    public static InputValidator getINSTANCE(){
+        if (INSTANCE == null) {
+            INSTANCE = new InputValidator();
+        }
+        return INSTANCE;
+    }
+
+    public boolean isEmailValid(String email) {
+        String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+
+    public boolean isInputNotEmpty(String syote) {
+        return !syote.isEmpty();
+    }
+
+}
